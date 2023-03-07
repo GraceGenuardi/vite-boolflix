49394b462840cdc74dead86deea343686d92b567
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-07 17:01:08 +0100

    EDIT Milstone 2

--- vite-boolfix/src/components/Main.vue
+++ vite-boolfix/src/components/Main.vue
@@ -1,62 +1,89 @@
 <template>
     <main class="main">
-  <!-- SEARCHBAR -->
-   <div class="col-2 search"> 
-    
-    
-    <form @submit.prevent="searchMovies">
-             <input type="text" v-model="searchTerm" placeholder="Cerca un film">
-             <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-          </form>
-
-          <div v-if="movies.length">
-              <div v-for="movie in movies" :key="movie.id">
-                 <h2>{{ movie.title }}</h2>
-                 <p>{{ movie.original_title }}</p>
-                 <p>{{ movie.original_language }}</p>
-                 <p>{{ movie.vote_average }}</p>
-              </div>
-         </div>
-
-           
-     
-  
-  
-    </div>   
+      <!-- SEARCHBAR -->
+      <div class="col-2 search">
+        <form @submit.prevent="searchMedia">
+          <input type="text" v-model="searchTerm" placeholder="Cerca un film o una serie tv" />
+          <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+      </div>
+      <h2 class="text-light">Risultati della tua ricerca:</h2>
+  <div v-if="mediaResults.length" class="text-light d-flex m-5">
+    <div v-for="media in mediaResults" :key="media.id" class="mx-3">
+      <h2>{{ media.title || media.name }}</h2>
+      <p>{{ media.original_title || media.original_name }}</p>
+      <p v-if="media.original_language">
+        <i v-if="getFlag(media.original_language)" :class="getFlag(media.original_language)"></i>
+        {{ media.original_language }}
+      </p>
+      <p>{{ media.vote_average }}</p>
+    </div>
+  </div>
     </main>
-  
   </template>
-  
   <script>
+  import axios from "axios";
   
-  import axios from 'axios';
-
   export default {
     data() {
       return {
-        searchTerm: '',
-        movies: []
-      }
+        searchTerm: "",
+        mediaResults: [],
+      };
     },
-
-    methods: {
-      searchMovies() {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-          params: {
-            api_key: 'c706f94ce5d14347af05f5cc11f3de87',
-            query: this.searchTerm
-          }
-        })
-        .then(response => {
-          this.movies = response.data.results;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-      }
-    }
-  }
   
+    methods: {
+      searchMedia() {
+        axios
+          .all([
+            axios.get("https://api.themoviedb.org/3/search/movie", {
+              params: {
+                api_key: "c706f94ce5d14347af05f5cc11f3de87",
+                query: this.searchTerm,
+                language: "it-IT",
+              },
+            }),
+            axios.get("https://api.themoviedb.org/3/search/tv", {
+              params: {
+                api_key: "c706f94ce5d14347af05f5cc11f3de87",
+                query: this.searchTerm,
+                language: "it-IT",
+              },
+            }),
+          ])
+          .then(
+            axios.spread((moviesRes, tvShowsRes) => {
+              const movies = moviesRes.data.results.map((movie) => ({
+                ...movie,
+                media_type: "movie",
+              }));
+              const tvShows = tvShowsRes.data.results.map((tvShow) => ({
+                ...tvShow,
+                media_type: "tv",
+              }));
+              this.mediaResults = [...movies, ...tvShows];
+            })
+          )
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+  
+      getFlag(language) {
+        switch (language) {
+          case "it":
+            return "flag-icon flag-icon-it";
+          case "en":
+            return "flag-icon flag-icon-gb";
+          case "fr":
+            return "flag-icon flag-icon-fr";
+          
+          default:
+            return "";
+        }
+      },
+    },
+  };
   </script>
   
   <style lang="scss" scoped>
