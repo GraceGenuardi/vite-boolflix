1156200f93ff91ba1ae2407a46b565de59ebb5e4
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-07 15:50:35 +0100

    Creato input di ricerca (Milstone 1)

--- vite-boolfix/src/components/Header.vue
+++ vite-boolfix/src/components/Header.vue
@@ -1,45 +1,64 @@
 <template>
     
-      <header class="main-header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">BOOLFLIX</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+    <header class="main-header">
+
+     
+        <div class="row">
+            
+     
+         <!-- NAVBAR -->
+          <nav class="navbar navbar-expand-lg navbar-light bg-light mx-5">
+            <a class="navbar-brand text-danger" href="#">BOOLFLIX</a>
+              <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+                  <span class="navbar-toggler-icon"></span>
+            </button>
   
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
-              </li>
-            </ul>
-  
-            <form class="form-inline my-2 my-lg-0" @submit.prevent="searchMovies">
-              <input class="form-control mr-sm-2" type="search" placeholder="Cerca un film" aria-label="Search" v-model="searchTerm" />
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cerca</button>
-            </form>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Serie tv</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Film</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Nuovi e Popolari</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">La mia lista</a>
+                </li>
+              </ul>
           </div>
-        </nav>
-      </header>
-    </template>
+         </nav>
+
+        
+
+
+      </div>
+     
+    </header>
+</template>
   
   <script>
-    
-  </script>
+  
+</script>
+
   
   <style lang="scss" scoped>
   @use '../style/partials/variables' as *;
   .main-header {
     text-align: left;
-    font-size: 10px;
     background-color: white;
     padding: 5px;
     font-size: 15px;
@@ -51,4 +70,6 @@
     width: 100px;
     padding-right: 10px;
   }
+
+
   </style>
--- vite-boolfix/src/components/Main.vue
+++ vite-boolfix/src/components/Main.vue
@@ -1,27 +1,80 @@
 <template>
     <main class="main">
-  
+  <!-- SEARCHBAR -->
+   <div class="col-2 search"> 
+    
+    
+    <form @submit.prevent="searchMovies">
+             <input type="text" v-model="searchTerm" placeholder="Cerca un film">
+             <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+          </form>
+
+          <div v-if="movies.length">
+              <div v-for="movie in movies" :key="movie.id">
+                 <h2>{{ movie.title }}</h2>
+                 <p>{{ movie.original_title }}</p>
+                 <p>{{ movie.original_language }}</p>
+                 <p>{{ movie.vote_average }}</p>
+              </div>
+         </div>
+
+           
      
   
   
-      
+    </div>   
     </main>
   
   </template>
   
   <script>
-  import axios from 'axios'
+  
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        searchTerm: '',
+        movies: []
+      }
+    },
+
+    methods: {
+      searchMovies() {
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: 'c706f94ce5d14347af05f5cc11f3de87',
+            query: this.searchTerm
+          }
+        })
+        .then(response => {
+          this.movies = response.data.results;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+    }
+  }
   
   </script>
   
   <style lang="scss" scoped>
   .main {
     padding: 50px 0;
-    background-color: rgb(177, 12, 12);
+    background-color: rgb(15, 11, 11);
   }
   .container-main{
     width: 1200px;
     margin-left: 350px;
+  }
+
+  .search {
+    position: relative;
+   
+    left: 1600px;
+    bottom: 100px;
+    
   }
   
   </style>
