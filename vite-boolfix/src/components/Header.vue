592d6da8f2422ef977ea89baee3f1d5382ac6200
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-07 14:45:26 +0100

    EDIT strttuta input

--- vite-boolfix/src/components/Header.vue
+++ vite-boolfix/src/components/Header.vue
@@ -1,11 +1,35 @@
 <template>
-    <header class="main-header">
-      <div class="container">
-        
-      </div>
+    
+      <header class="main-header">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">BOOLFLIX</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
   
-    </header>
-  </template>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+            </ul>
+  
+            <form class="form-inline my-2 my-lg-0" @submit.prevent="searchMovies">
+              <input class="form-control mr-sm-2" type="search" placeholder="Cerca un film" aria-label="Search" v-model="searchTerm" />
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cerca</button>
+            </form>
+          </div>
+        </nav>
+      </header>
+    </template>
   
   <script>
     
@@ -16,10 +40,11 @@
   .main-header {
     text-align: left;
     font-size: 10px;
-    background-color: $bg-color;
+    background-color: white;
     padding: 5px;
     font-size: 15px;
     color: black;
+    width: 1920px;
     
   }
   .logo {
