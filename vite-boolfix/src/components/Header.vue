f9de12d279b5523846bc0e0648c1568fd4d0f472
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-07 13:30:45 +0100

    START EDIT aggiunti componenti

---
+++ vite-boolfix/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="main-header">
+      <div class="container">
+        
+      </div>
+  
+    </header>
+  </template>
+  
+  <script>
+    
+  </script>
+  
+  <style lang="scss" scoped>
+  @use '../style/partials/variables' as *;
+  .main-header {
+    text-align: left;
+    font-size: 10px;
+    background-color: $bg-color;
+    padding: 5px;
+    font-size: 15px;
+    color: black;
+    
+  }
+  .logo {
+    width: 100px;
+    padding-right: 10px;
+  }
+  </style>
---
+++ vite-boolfix/src/components/Main.vue
@@ -0,0 +1,58 @@
+<template>
+    <main class="main">
+  
+     
+  
+  
+      
+    </main>
+  
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  </script>
+  
+  <style lang="scss" scoped>
+  .main {
+    padding: 50px 0;
+    background-color: black;
+  }
+  .container-main{
+    width: 1200px;
+    margin-left: 350px;
+  }
+  .cards {
+    display: grid;
+    grid-template-columns: repeat(4,1fr);
+    background-color: rgba(255, 255, 255, 0.586);
+    padding: 50px;
+  }
+  .found{
+    color: white;
+    position: relative;
+    width: 1075px;
+    left: 70px;
+    top: 75px;
+    background-color: rgb(47, 47, 47);
+    text-align: left;
+    padding: 10px;
+    
+  }
+  /**MENU RULES */
+  .filter {
+    background-color: rgb(255, 104, 10);
+    margin-bottom: 25px;
+    display: flex;
+    width: 220px;
+    margin-left: 360px;
+    padding: 5px;
+    border-radius: 5px;
+  }
+  .filter i{
+    color: white;
+    margin-top: 3px;
+    margin-right: 3px;
+  }
+  </style>
